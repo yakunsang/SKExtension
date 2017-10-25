0dba1c98cec3e60213a0f8ce675a1747cf06329e
yakunsang <378082759@qq.com>
2017-10-25 19:15:09 +0800

    no message

--- SKExtension/Addition/UIKit/UIViewController+SKMediator.m
+++ SKExtension/Addition/UIKit/UIViewController+SKMediator.m
@@ -34,7 +34,7 @@
 	[self.navigationController popToRootViewControllerAnimated:animated];
 }
 
-- (void)sk_popToViewController:(NSString *)viewController animated:(BOOL)animated {
+- (void)sk_popToViewController:(NSString *_Nonnull)viewController animated:(BOOL)animated {
 	[self sk_popToViewController:viewController params:@{} animated:animated];
 }
 
